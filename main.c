d06a6959f936737d07b9182ec36e8791feb5d671
Jayakrishnan S <72574346+JK432@users.noreply.github.com>
2021-12-31 10:50:44 +0000

    test clear

--- main.c
+++ main.c
@@ -7,54 +7,50 @@ char name[50];
 int rollno;
 int mark[5];
 int total;
+char grade;
 }jk[100];
 int n;
  
-printf("Enter the number of students\n");
+// printf("Enter the number of students\n");
 scanf("%d",&n);
 for(int i=0;i<n;i++){
-  printf("Enter the name of student\n");
-  scanf("%s",jk[i].name);
-  printf("Enter the rollno of student\n");
+    // printf("Enter the rollno of student\n");
   scanf("%d",&jk[i].rollno);
-  printf("Enter the marks for five subjects out of 100\n");
+  // printf("Enter the name of student\n");
+  scanf("%s",jk[i].name);
+
+  // printf("Enter the marks for five subjects out of 100\n");
   int sum =0;
-  for(int j=0;j<5;j++){
+  for(int j=0;j<3;j++){
     scanf("%d",&jk[i].mark[j]);
     sum=sum+jk[i].mark[j];
   }
-  float avg = sum/5;
-  printf("Total marks is %d\n ",sum);
-  if(avg<=100 && avg>=90){
-    printf("Grade: A+\n");
+   jk[i].total=sum;
+  int avg = ((jk[i].total/150)*100);
+  // printf("%d",avg);
+  // printf("Total marks is %d\n ",sum);
+  if((sum<=150) && (sum>=130)){
+    jk[i].grade='A';
+
   }
-    else if(avg<90 && avg>=80){
-    printf("Grade: A\n");
+    else if((sum<130) && (sum>=120)){
+    jk[i].grade='B';
   }
-    else if(avg<80 && avg>=70){
-    printf("Grade: B+ \n");
-  }
-    else if(avg<70 && avg>=60){
-    printf("Grade: B\n");
-  }
-    else if(avg<60 && avg>=50){
-    printf("Grade: C+ \n");
-  }
-    else if(avg<50 && avg>=40){
-    printf("Grade: C \n");
-  }
-    else if(avg<40 && avg>=30){
-    printf("Grade: D+ \n");
-  }
-    else if(avg<30 && avg>=20){
-    printf("Grade: D \n");
-  }
-    else{
-    printf("Grade: Failure is the key to sucess\n");
-  }
+    else if((sum<120) && (sum>=100)){
+    jk[i].grade='C';
   
+  }
+    else if((sum<110) && (sum>=90)){
+    jk[i].grade='D';
+  }
+    
   
 
 }
+printf("Roll No  Name     Total Marks   Grade");
+for(int i=0;i<n;i++ ){
+  
+  printf("\n%-9d%-11s%-13d%c",jk[i].rollno,jk[i].name,jk[i].total,jk[i].grade);
 
+}
 }
